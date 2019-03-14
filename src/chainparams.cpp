560b427a76bb46941f8314b91e223e4452af8891
skypromas <zihau96@hotmail.com>
2019-03-14 11:02:30 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -81,7 +81,7 @@ public:
         consensus.BIP34Hash = uint256S("0x000000000ded045d1bf708373e732fb5562303e876612126b7e9ced983e3c7a4");
         consensus.BIP65Height = 0; // 000000000000000004c2b624ed5d7756c508d90fd0da2c7c679febfa6c4735f0
         consensus.BIP66Height = 0; // 00000000000000000379eaa19dce8c9b722d46ae6a57c2f1a988119488b50931
-        consensus.powLimit = uint256S("0x00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
         consensus.nPowTargetTimespan = 2/3 * 60 * 60; // 40 minute
         consensus.nPowTargetSpacing = 1 * 60;
         consensus.fPowAllowMinDifficultyBlocks = false;
