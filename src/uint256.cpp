aa335e98b8ebf29e88526eb93f422bdd7cdb0d5a
skypromas <zihau96@hotmail.com>
2020-06-01 14:31:42 +0800

    Update uint256.cpp

--- src/uint256.cpp
+++ src/uint256.cpp
@@ -18,7 +18,7 @@ base_blob<BITS>::base_blob(const std::vector<unsigned char>& vch)
 }
 
 template <unsigned int BITS>
-std::string base_blob<BITS>::GetHex() const
+std::string base_blob<BITS>::GetHex() 
 {
     return HexStr(std::reverse_iterator<const uint8_t*>(data + sizeof(data)), std::reverse_iterator<const uint8_t*>(data));
 }
@@ -66,14 +66,14 @@ std::string base_blob<BITS>::ToString() const
 
 // Explicit instantiations for base_blob<160>
 template base_blob<160>::base_blob(const std::vector<unsigned char>&);
-template std::string base_blob<160>::GetHex() const;
+template std::string base_blob<160>::GetHex() ;
 template std::string base_blob<160>::ToString() const;
 template void base_blob<160>::SetHex(const char*);
 template void base_blob<160>::SetHex(const std::string&);
 
 // Explicit instantiations for base_blob<256>
 template base_blob<256>::base_blob(const std::vector<unsigned char>&);
-template std::string base_blob<256>::GetHex() const;
+template std::string base_blob<256>::GetHex() ;
 template std::string base_blob<256>::ToString() const;
 template void base_blob<256>::SetHex(const char*);
 template void base_blob<256>::SetHex(const std::string&);
