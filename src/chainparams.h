1cb8c8bf088170818a6756bc246b3259c5693f94
skypromas <zihau96@hotmail.com>
2020-06-01 14:09:19 +0800

    Update chainparams.h

--- src/chainparams.h
+++ src/chainparams.h
@@ -104,7 +104,7 @@ std::unique_ptr<CChainParams> CreateChainParams(const std::string& chain);
  * Return the currently selected parameters. This won't change after app
  * startup, except for unit tests.
  */
-const CChainParams &Params();
+CChainParams &Params();
 
 /**
  * Sets the params returned by Params() to those for the given BIP70 chain name.
